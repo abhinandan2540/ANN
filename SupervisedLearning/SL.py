2c13f08c70ba309e882abecaafe02c67c81ae920
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-08-25 23:00:03 +0530

    SL-concepts

---
+++ SupervisedLearning/SL.py
@@ -0,0 +1,43 @@
+
+"""
+Supervised Learning mainly 2 types
+1. Classification (defined labels, for categorical variables)
+2. Regression (no defined labels, for continuous variables)
+
+
+
+1. Classification
+--------------------------------------
+For linear classifier, we've (linearly seperable decision boundary between classes)
+1. logistic regression (binomial, multinomial)
+2. SVM
+3. single layer perceptron
+
+for non-linear classifier, we've (not linearly seperable decision boundary between classes)
+1. KNN
+2. Decision Tree
+3. Random Forest
+4. Multilayer Neural Network
+
+
+2. Regression
+--------------------------------------
+1. Simple linear regression
+2. Multiple linear regression
+3. Polynomial regression
+4. Ridge, Lasso regressor (L2, L1)
+5. Decision Tree regressor
+6. Random Forest regressor
+
+
+
+Regression evaluation metric
+------------------------------
+1. MSE
+2. MAE
+3. RMSE
+4. Huber Loss
+5. R2-Score
+
+
+"""
