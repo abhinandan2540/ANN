a80a2f16a39c090b2065ab0bb93886f93e5a4da4
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-08-25 23:00:16 +0530

    RMSE

---
+++ SupervisedLearning/RMSE.py
@@ -0,0 +1,9 @@
+
+import math
+actual = [2, 3, 4, 5, 5]
+predicted = [1, 1, 2, 2, 3]
+
+error = 0
+for i in range(5):
+    error += (abs(actual[i]-predicted[i]))**2
+print(math.sqrt(error/len(actual)))
